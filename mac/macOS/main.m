ee9d23c12ce79abc5be5181ef6f794801704f948
Mike Simms <msimms01@gmail.com>
2022-05-08 15:42:23 -0400

    More rearranging.

---
+++ mac/macOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BluetoothTest_objc
+//
+//  Created by Michael Simms on 5/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
