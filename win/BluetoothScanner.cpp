fa8a81f35a03076a1d2126842e8ecbcebd717352
Mike Simms <msimms01@gmail.com>
2022-05-12 09:37:06 -0400

    Backup commit of Windows implementation.

--- win/BluetoothScanner.cpp
+++ win/BluetoothScanner.cpp
@@ -1,7 +1,9 @@
 #include "BluetoothScanner.h"
 #include <devguid.h>
 #include <bthdef.h>
+#include <bthsdpdef.h>
 #include <Bluetoothleapis.h>
+#include <bluetoothapis.h>
 
 BluetoothScanner::BluetoothScanner()
 {
@@ -13,6 +15,57 @@ BluetoothScanner::~BluetoothScanner()
 
 void BluetoothScanner::startScanning()
 {
+	while (TRUE)
+	{
+		BLUETOOTH_FIND_RADIO_PARAMS radioFindParams;
+		radioFindParams.dwSize = sizeof(BLUETOOTH_FIND_RADIO_PARAMS);
+
+		HANDLE hRadio = NULL;
+		HBLUETOOTH_RADIO_FIND hBtRadioFind = BluetoothFindFirstRadio(&radioFindParams, &hRadio);
+		if (hBtRadioFind)
+		{
+			do
+			{
+				BLUETOOTH_RADIO_INFO radioInfo;
+				ZeroMemory(&radioInfo, sizeof(BLUETOOTH_RADIO_INFO));
+				radioInfo.dwSize = sizeof(BLUETOOTH_RADIO_INFO);
+
+				DWORD ret = BluetoothGetRadioInfo(hRadio, &radioInfo);
+				if (ret == ERROR_SUCCESS)
+				{
+					BLUETOOTH_DEVICE_SEARCH_PARAMS searchParams;
+					ZeroMemory(&searchParams, sizeof(BLUETOOTH_DEVICE_SEARCH_PARAMS));
+					searchParams.dwSize = sizeof(BLUETOOTH_DEVICE_SEARCH_PARAMS);
+					searchParams.fReturnAuthenticated = TRUE;
+					searchParams.fReturnConnected = TRUE;
+					searchParams.fReturnRemembered = TRUE;
+					searchParams.fReturnUnknown = TRUE;
+					searchParams.fIssueInquiry = TRUE;
+					searchParams.cTimeoutMultiplier = 15;
+					searchParams.hRadio = hRadio;
+
+					BLUETOOTH_DEVICE_INFO deviceInfo;
+					ZeroMemory(&deviceInfo, sizeof(BLUETOOTH_DEVICE_INFO));
+					deviceInfo.dwSize = sizeof(BLUETOOTH_DEVICE_INFO);
+
+					HBLUETOOTH_DEVICE_FIND hBthDeviceFind = BluetoothFindFirstDevice(&searchParams, &deviceInfo);
+					if (hBthDeviceFind)
+					{
+						do
+						{
+						} while (BluetoothFindNextDevice(hBthDeviceFind, &deviceInfo));
+
+						BluetoothFindDeviceClose(hBthDeviceFind);
+					}
+				}
+
+				CloseHandle(hRadio);
+				hRadio = NULL;
+			} while (BluetoothFindNextRadio(hBtRadioFind, &hRadio));
+
+			BluetoothFindRadioClose(hBtRadioFind);
+		}
+	}
 }
 
 void BluetoothScanner::stopScanning()
@@ -44,8 +97,13 @@ void BluetoothScanner::didDiscover(HANDLE hDevice)
 				RtlZeroMemory(pCharBuffer, charBufferSize * sizeof(BTH_LE_GATT_CHARACTERISTIC));
 
 				USHORT numChars = 0;
+				USHORT charBufferSize = 0;
 
 				hr = BluetoothGATTGetCharacteristics(hDevice, pServiceBuffer, charBufferSize, pCharBuffer, &numChars, BLUETOOTH_GATT_FLAG_NONE);
+
+				for (USHORT i = 0; i < charBufferSize; ++i)
+				{
+				}
 			}
 		}
 	}
