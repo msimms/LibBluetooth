48242073b4e8703063361e60f5d4483087f96759
Mike Simms <msimms01@gmail.com>
2022-08-17 14:21:02 -0400

    Very beginnings of an Android version.

---
+++ android/BluetoothScanner.java
@@ -0,0 +1,17 @@
+//
+//  BluetoothScanner.java
+//  Created by Michael Simms on 8/17/22.
+//
+
+import javax.bluetooth.*
+
+class BluetoothScanner {
+    void start() {
+    }
+
+    void wait() {
+    }
+
+    void stop() {
+    }
+}
