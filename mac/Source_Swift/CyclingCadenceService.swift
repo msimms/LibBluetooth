b55a4b8c97627dae37c159c6f6e26ad91f23e844
Mike Simms <msimms01@gmail.com>
2022-12-29 16:21:44 -0500

    Fixed the cycling cadende parser in Swift.

--- mac/Source_Swift/CyclingCadenceService.swift
+++ mac/Source_Swift/CyclingCadenceService.swift
@@ -34,7 +34,7 @@ struct RevMeasurement {
 }
 
 func decodeCyclingCadenceReading(data: Data) throws -> Dictionary<String, UInt32> {
-	if data.count < 11 {
+	if data.count < 5 {
 		throw CyclingCadenceException.runtimeError("Not enough data")
 	}
 
@@ -44,26 +44,23 @@ func decodeCyclingCadenceReading(data: Data) throws -> Dictionary<String, UInt32
 	var revData = RevMeasurement()
 
 	cscData.flags = data[0]
-	cscData.cumulativeWheelRevs = ((UInt32)(data[1]) << 24) | ((UInt32)(data[2]) << 16) | ((UInt32)(data[3]) << 8) | (UInt32)(data[4])
-	cscData.lastWheelEventTime = ((UInt16)(data[5]) << 8) | (UInt16)(data[6])
-	cscData.cumulativeCrankRevs = ((UInt16)(data[7]) << 8) | (UInt16)(data[8])
-	cscData.lastCrankEventTime = ((UInt16)(data[9]) << 8) | (UInt16)(data[10])
-
 	revData.flags = data[0]
-	revData.cumulativeCrankRevs = ((UInt16)(data[1]) << 8) | (UInt16)(data[2])
-	revData.lastCrankEventTime = ((UInt16)(data[3]) << 8) | (UInt16)(data[4])
 
-	if (cscData.flags & WHEEL_REVOLUTION_DATA_PRESENT != 0)
-	{
+	if cscData.flags & WHEEL_REVOLUTION_DATA_PRESENT != 0 {
+		cscData.cumulativeWheelRevs = ((UInt32)(data[1]) << 24) | ((UInt32)(data[2]) << 16) | ((UInt32)(data[3]) << 8) | (UInt32)(data[4])
+		cscData.lastWheelEventTime = ((UInt16)(data[5]) << 8) | (UInt16)(data[6])
 		result[KEY_NAME_WHEEL_REV_COUNT] = UInt32(CFSwapInt32BigToHost(cscData.cumulativeWheelRevs))
 	}
-	if (cscData.flags & CRANK_REVOLUTION_DATA_PRESENT != 0)
-	{
+	if cscData.flags & CRANK_REVOLUTION_DATA_PRESENT != 0 {
 		if data.count > 5 {
+			cscData.cumulativeCrankRevs = ((UInt16)(data[7]) << 8) | (UInt16)(data[8])
+			cscData.lastCrankEventTime = ((UInt16)(data[9]) << 8) | (UInt16)(data[10])
 			result[KEY_NAME_WHEEL_CRANK_COUNT] = UInt32(CFSwapInt16BigToHost(cscData.cumulativeCrankRevs))
 			result[KEY_NAME_WHEEL_CRANK_TIME] = UInt32(CFSwapInt16BigToHost(cscData.lastCrankEventTime))
 		}
 		else {
+			revData.cumulativeCrankRevs = ((UInt16)(data[1]) << 8) | (UInt16)(data[2])
+			revData.lastCrankEventTime = ((UInt16)(data[3]) << 8) | (UInt16)(data[4])
 			result[KEY_NAME_WHEEL_CRANK_COUNT] = UInt32(CFSwapInt16BigToHost(revData.cumulativeCrankRevs))
 			result[KEY_NAME_WHEEL_CRANK_TIME] = UInt32(CFSwapInt16BigToHost(revData.lastCrankEventTime))
 		}
